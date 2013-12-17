0c0df742436b8a1a62bef125fe430c43813ee192
Ed Coyne <edcoyne@google.com>
2013-12-17 11:23:01 -0800

    Checking in ui stubs

---
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/ui/EditFragmentNestLocation.java
@@ -0,0 +1,17 @@
+package com.islandturtlewatch.nest.reporter.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.islandturtlewatch.nest.reporter.R;
+
+public class EditFragmentNestLocation extends EditFragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+  		ViewGroup container,
+      Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.edit_fragment_nest_location, container, false);
+  }
+}
