07f95465a905227163494127b01dc25c5a65d12b
Ed Coyne <edcoyne@google.com>
2013-12-16 20:15:20 -0800

    Basic display ui

--- client/android/src/src/com/islandturtlewatch/nest/reporter/ui/EditFragment.java
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/ui/EditFragment.java
@@ -1,7 +1,19 @@
 package com.islandturtlewatch.nest.reporter.ui;
 
+import com.islandturtlewatch.nest.reporter.R;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class EditFragment extends Fragment {
-
+	
+	 @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container,
+       Bundle savedInstanceState) {
+       // Inflate the layout for this fragment
+       return inflater.inflate(R.layout.edit_fragment_info, container, false);
+   }
 }
