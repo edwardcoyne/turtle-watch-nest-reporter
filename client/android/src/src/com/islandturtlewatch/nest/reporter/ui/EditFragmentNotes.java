8075ebb9f4d700624a95857ac354c007cacdeed8
Ed Coyne <edcoyne@google.com>
2014-03-21 17:04:57 -0700

    added files that should have been added a while back

---
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/ui/EditFragmentNotes.java
@@ -0,0 +1,45 @@
+package com.islandturtlewatch.nest.reporter.ui;
+
+import java.util.Map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.islandturtlewatch.nest.data.ReportProto.Report;
+import com.islandturtlewatch.nest.reporter.EditPresenter.DataUpdateHandler;
+import com.islandturtlewatch.nest.reporter.R;
+
+public class EditFragmentNotes extends EditFragment {
+  private static final Map<Integer, TextChangeHandler> TEXT_CHANGE_HANDLERS =
+      TextChangeHandler.toMap(new HandleUpdateNotes());
+
+  @Override
+  public Map<Integer, TextChangeHandler> getTextChangeHandlers() {
+    return TEXT_CHANGE_HANDLERS;
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+      ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.edit_fragment_notes, container, false);
+  }
+
+  @Override
+  public void updateSection(Report report) {
+    setText(R.id.fieldNotes, report.getAdditionalNotes());
+  }
+
+  private static class HandleUpdateNotes extends TextChangeHandler {
+    protected HandleUpdateNotes() {
+      super(R.id.fieldNotes);
+    }
+
+    @Override
+    public void handleTextChange(String newText, DataUpdateHandler updateHandler) {
+      updateHandler.updateNotes(newText);
+    }
+  }
+}
---
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/ui/FocusMonitoredEditText.java
@@ -0,0 +1,40 @@
+package com.islandturtlewatch.nest.reporter.ui;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.widget.EditText;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.islandturtlewatch.nest.reporter.EditPresenter.DataUpdateHandler;
+import com.islandturtlewatch.nest.reporter.ui.EditFragment.TextChangeHandlerSimple;
+
+public class FocusMonitoredEditText extends EditText {
+  private Optional<TextChangeHandlerSimple> handler = Optional.absent();
+  private Optional<DataUpdateHandler> updateHandler = Optional.absent();
+
+  public FocusMonitoredEditText(Context context) {
+    super(context);
+  }
+
+  public void setTextChangeHandler(TextChangeHandlerSimple handler) {
+    this.handler = Optional.of(handler);
+  }
+
+  public void setDataUpdateHandler(DataUpdateHandler handler) {
+    this.updateHandler = Optional.of(handler);
+  }
+
+  @Override
+  protected void onFocusChanged(boolean focused, int direction,
+      Rect previouslyFocusedRect) {
+    super.onFocusChanged(focused, direction, previouslyFocusedRect);
+    if (focused) {
+      return;
+    }
+    if (handler.isPresent()) {
+      Preconditions.checkArgument(updateHandler.isPresent(), "must call setDataUpdateHandler()");
+      handler.get().handleTextChange(getText().toString(), updateHandler.get());
+    }
+  }
+}
