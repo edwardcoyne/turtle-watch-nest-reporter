72a557cb6cbcf38a07a0a61ec36a34bd2ae64cfe
Ed Coyne <edcoyne@google.com>
2014-05-10 13:59:51 -0700

    Added run environment to control local/prod

---
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/RunEnvironment.java
@@ -0,0 +1,31 @@
+package com.islandturtlewatch.nest.reporter;
+
+import android.os.Build;
+
+import com.islandturtlewatch.nest.reporter.transport.reportEndpoint.ReportEndpoint;
+
+public class RunEnvironment {
+  private enum Environment{
+    LOCAL,
+    DEVELOPMENT,
+    PROD
+  };
+  private static final Environment environment = Environment.LOCAL;
+  private static final String localAddress = "10.255.0.43:8888";
+
+  public static String getRootBackendUrl() {
+    switch(environment) {
+      case LOCAL:
+        return "http://" + localAddress + "/_ah/api";
+      case DEVELOPMENT:
+      case PROD:
+        return ReportEndpoint.DEFAULT_ROOT_URL;
+      default:
+        throw new UnsupportedOperationException("Don't support environment: " + environment);
+    }
+  }
+
+  public static boolean isEmulator() {
+    return Build.PRODUCT.contains("sdk");
+  }
+}
