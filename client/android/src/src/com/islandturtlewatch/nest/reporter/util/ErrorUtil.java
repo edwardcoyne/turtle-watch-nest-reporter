d327f44c82d9949d01ab7c15c82f36a49ea44a9f
Ed Coyne <edcoyne@google.com>
2014-05-26 18:06:08 -0700

    fixed pictures on emulator

--- client/android/src/src/com/islandturtlewatch/nest/reporter/util/ErrorUtil.java
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/util/ErrorUtil.java
@@ -2,6 +2,7 @@ package com.islandturtlewatch.nest.reporter.util;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -9,8 +10,21 @@ public class ErrorUtil {
   private static final String TAG = ErrorUtil.class.getSimpleName();
   private ErrorUtil() {} // Only static
 
-  public static void showErrorMessage(Fragment fragment, String message) {
-    showErrorMessage(fragment.getActivity(), message);
+  public static void showErrorMessage(final Fragment fragment, final String message) {
+    if (fragment.getActivity() == null) {
+      new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected void onPostExecute(Void params) {
+          showErrorMessage(fragment.getActivity(), message);
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+          return null;
+        }
+      };
+    } else {
+      showErrorMessage(fragment.getActivity(), message);
+    }
   }
 
   public static void showErrorMessage(Context context, String message) {
