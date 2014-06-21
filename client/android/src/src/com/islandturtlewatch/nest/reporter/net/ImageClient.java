76aebb86608b524e3184689dd73d9011e4478a26
Ed Coyne <edcoyne@google.com>
2014-06-21 13:30:29 -0700

    moved back from 1.0

---
+++ client/android/src/src/com/islandturtlewatch/nest/reporter/net/ImageClient.java
@@ -0,0 +1,17 @@
+package com.islandturtlewatch.nest.reporter.net;
+
+import android.content.Context;
+
+import com.islandturtlewatch.nest.data.ImageProto.ImageRef;
+
+public class ImageClient {
+  //ImageEndpoint imageService;
+  Context context;
+
+  private void initService() {
+
+  }
+
+  public void uploadImage(Context context, ImageRef ref) {
+  }
+}
