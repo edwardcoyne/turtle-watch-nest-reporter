ee04258ab9aeab7adf741281e9414b7c000fc648
ed <cap2501@gmail.com>
2020-09-04 13:04:45 -0700

    Add python protos

--- proto/gen_proto.sh
+++ proto/gen_proto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-protoc --java_out=./generated report.proto 
-protoc --java_out=./generated image.proto 
+protoc --java_out=./generated --python_out=./generated report.proto 
+protoc --java_out=./generated --python_out=./generated image.proto 
